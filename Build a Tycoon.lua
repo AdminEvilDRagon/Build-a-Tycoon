585ff85aaedae803eb2c24cec901a3d01781406e
AdminEvilDRagon <92892215+AdminEvilDRagon@users.noreply.github.com>
2024-08-06 13:13:53 +0200

    Build a Tycoon.lua

--- Build a Tycoon.lua
+++ Build a Tycoon.lua
@@ -1,12 +1,14 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Build a Tycoon", HidePremium = false, SaveConfig = true, ConfigFolder = "MyConfig"})
+local Window = OrionLib:MakeWindow({Name = "Build a Tycoon!", HidePremium = false, SaveConfig = true, ConfigFolder = "MyConfig"})
 
 -- Tabs
 local MoneyTab = Window:MakeTab({Name = "Money", Icon = "rbxassetid://4483345998", PremiumOnly = false})
 local RebirthTab = Window:MakeTab({Name = "Rebirth", Icon = "rbxassetid://4483345998", PremiumOnly = false})
 local DropperTab = Window:MakeTab({Name = "Dropper", Icon = "rbxassetid://4483345998", PremiumOnly = false})
 local SpotTab = Window:MakeTab({Name = "Spots", Icon = "rbxassetid://4483345998", PremiumOnly = false})
-local UpgradeTab = Window:MakeTab({Name = "Tokens", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+local UpgradeTab = Window:MakeTab({Name = "Upgrades", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+local TitleTab = Window:MakeTab({Name = "Title", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+local CreditsTab = Window:MakeTab({Name = "Credits", Icon = "rbxassetid://4483345998", PremiumOnly = false})
 
 -- Functions
 local function getMoney(amount)
@@ -36,6 +38,11 @@ end
 local function upgradeTime(amount)
     local args = {[1] = -amount, [2] = 1, [3] = "Multi"}
     workspace:WaitForChild("MainScripts"):WaitForChild("RemoteScripts"):WaitForChild("TimeUpgrade"):FireServer(unpack(args))
+end
+
+local function changeTitle(title)
+    local args = {[1] = title}
+    workspace:WaitForChild("MainScripts"):WaitForChild("RemoteScripts"):WaitForChild("ChangeTitle"):FireServer(unpack(args))
 end
 
 -- UI Elements
@@ -70,13 +77,26 @@ SpotTab:AddButton({
 })
 
 UpgradeTab:AddTextbox({
-    Name = "Time Upgrade Tokens",
+    Name = "Time Upgrade Amount",
     Default = "",
     TextDisappear = true,
     Callback = function(value)
         upgradeTime(tonumber(value))
     end
 })
+
+TitleTab:AddDropdown({
+    Name = "Select Title",
+    Default = "CREATOR",
+    Options = {"CREATOR", "VIP", "DEV", "MILLIONAIRE", "BILLIONAIRE", "TRILLIONAIRE", "OBBYIST", "QUADRILLIONAIRE", "QUINTILLIONAIRE", "MEOW", "BOLLY", "CONTRIBUTOR", "OUT OF BOUNDS", "SILLY", "ASTRONAUT", "ALIEN", "MODERATOR", "TITLEIST", "METEOROLOGIST", "HI", "VOID"},
+    Callback = function(value)
+        changeTitle(value)
+    end
+})
+
+-- Credits
+CreditsTab:AddParagraph("Creator", "Evildragon")
+CreditsTab:AddParagraph("Thanks", "@Sillyfrfr for finding the Title remote, follow him on scriptblox.com")
 
 -- Infinite Yield
 local InfiniteYieldTab = Window:MakeTab({Name = "Infinite Yield", Icon = "rbxassetid://4483345998", PremiumOnly = false})
